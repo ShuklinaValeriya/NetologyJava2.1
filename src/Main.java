cb78a742772f179c02f009635df4ad1d8866e2c3
unknown <shuklinavaleria@yandex.ru>
2022-02-14 17:19:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 10000;
+        int price1miles = 20;
+
+        int miles = priceTicket / price1miles;
+        System.out.println("Мы начисли Вам аж " + miles + " мили");
+    }
+}
